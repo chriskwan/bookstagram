7c15e6bfd6de4eb112882b7dd5224a7156c45a3f
Chris Kwan <chriswkwan@gmail.com>
2017-02-11 14:19:28 -0500

    Add a placeholder image for BookTile

--- src/BookTile.js
+++ src/BookTile.js
@@ -4,6 +4,7 @@ class BookTile extends Component {
     render() {
         return (
             <div className="BookTile">
+                <img src={require('./cat_reading.jpg')} className="BookTileImage" />
                 Hey I'm a book with title: { this.props.title }!
             </div>
         );
