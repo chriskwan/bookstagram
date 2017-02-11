12b66bd7e4eef5ad0f80d9a92745c737d2a5dafd
Chris Kwan <chriswkwan@gmail.com>
2017-02-11 14:34:44 -0500

    Add Button bar component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import BookDetails from './BookDetails';
 import BookFeed from './BookFeed';
+import ButtonBar from './ButtonBar';
 import UserProfile from './UserProfile';
 
 class App extends Component {
@@ -47,17 +48,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>*/}
-
           { currentView }
-
-        <div>
-          <ul>
-            <li>Discover</li>
-            <li>Post</li>
-            <li>You</li>
-          </ul>
-        </div>
-
+          <ButtonBar />
       </div>
     );
   }
---
+++ src/ButtonBar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class ButtonBar extends Component {
+    render() {
+        return (
+            <div>
+                <ul>
+                    <li>Discover</li>
+                    <li>Post</li>
+                    <li>You</li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default ButtonBar;
