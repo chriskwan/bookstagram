88f263a91e989fa99b07841d67451cdb9573960c
Chris Kwan <chriswkwan@gmail.com>
2017-02-11 16:47:23 -0500

    Add bottom bar with icons

--- src/ButtonBar.js
+++ src/ButtonBar.js
@@ -1,26 +1,65 @@
 import React, { Component } from 'react';
+import {BottomNavigation, BottomNavigationItem} from 'material-ui/BottomNavigation';
+import Paper from 'material-ui/Paper';
+
+import Home from 'material-ui/svg-icons/action/home'
+import InsertEmoticon from 'material-ui/svg-icons/editor/insert-emoticon';
+import ModeEdit from 'material-ui/svg-icons/editor/mode-edit';
+
+const discoverIcon = <Home />;
+const postIcon = <ModeEdit />;
+const profileIcon = <InsertEmoticon />;
 
 class ButtonBar extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            selectedIndex: 0
+        };
+    }
+
     handlePostClick(e) {
         e.preventDefault();
         alert("Stay tuned!");
     }
 
+    selectButton(index) {
+        this.setState({
+            selectedIndex: index
+        })
+    }
+
     render() {
         return (
-            <div>
-                <ul>
-                    <li>
-                        <a href="" onClick={this.props.onChangeToFeed}>Discover</a>
-                    </li>
-                    <li>
-                        <a href="" onClick={this.handlePostClick}>Post</a>
-                    </li>
-                    <li>
-                        <a href="" onClick={this.props.onChangeToProfile}>You</a>
-                    </li>
-                </ul>
-            </div>
+            <Paper zDepth={1}>
+                <BottomNavigation selectedIndex={this.state.selectedIndex}>
+                    <BottomNavigationItem 
+                        label="Discover"
+                        icon={discoverIcon}
+                        onTouchTap={(e) => {
+                            this.selectButton(0);
+                            this.props.onChangeToFeed(e);
+                        }}
+                    />
+                    <BottomNavigationItem 
+                        label="Post"
+                        icon={postIcon}
+                        onTouchTap={(e) => {
+                            this.selectButton(1);
+                            this.handlePostClick(e);
+                        }}
+                    />
+                    <BottomNavigationItem
+                        label="You"
+                        icon={profileIcon}
+                        onTouchTap={(e) => {
+                            this.selectButton(2);
+                            this.props.onChangeToProfile(e);
+                        }}
+                    />
+                </BottomNavigation>
+            </Paper>
         );
     }
 }
