4a4a1b8811d97e0837b9d884d366debfd332fa36
Chris Kwan <chriswkwan@gmail.com>
2017-02-11 19:52:19 -0500

    Add initial User Profile elements

--- src/UserProfile.js
+++ src/UserProfile.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
+import Avatar from 'material-ui/Avatar';
+import RaisedButton from 'material-ui/RaisedButton';
+
+import placeholderImage from './cat_reading.jpg';
 
 class UserProfile extends Component {
     render() {
+        const userData = {
+            name: 'Ana',
+            numBooks: 422,
+            numFollowers: 273,
+            numFollowing: 99
+        };
+
+        const style = {
+            margin: 12,
+        };
+
         return (
             <div>
-                User Profile
+                {userData.name}
+                
+                <Avatar src={placeholderImage} />
+
+                
+                {userData.numBooks} books
+                {userData.numFollowers} followers
+                {userData.numFollowing} following
+
+                <RaisedButton label="edit profile" style={style}/>
             </div>
         );
     }
