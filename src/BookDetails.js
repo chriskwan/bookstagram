9e21d11e38a99a77f2a6f7f7a50a2c02e865f2ac
Chris Kwan <chriswkwan@gmail.com>
2017-02-12 10:29:25 -0500

    Add remaining data to BookDetails

--- src/BookDetails.js
+++ src/BookDetails.js
@@ -6,6 +6,7 @@ class BookDetails extends Component {
             image: './harrypotter.png',
             title: "Harry Potter and the Goblet of Fire",
             author: "J. K. Rowling",
+            blurb: "Harry Potter's 4th year of Hogwarts",
             description: "Sometimes you just need to read some Harry Potter. #resist #dumbledore #hp4 #dumbledorequotes",
             comments: [
                 "Jamie: Srsly",
@@ -18,13 +19,17 @@ class BookDetails extends Component {
 
         return (
             <div>
-                {bookData.title}
-                by {bookData.author}
-                {bookData.description}
-                {bookData.comments.map((comment) => {
-                    return <p key={comment}>{comment}</p>
-                })}
                 <img className="BookDetailsImage" src={require(bookData.image)} alt="book"/>
+                <div>{bookData.title}</div>
+                <div>{bookData.author}</div>
+                <div>{bookData.blurb}</div>
+                <div>{bookData.description}</div>
+                {/*{bookData.comments.map((comment) => {
+                    return <p key={comment}>{comment}</p>
+                })}*/}
+                {bookData.links.map((link) => {
+                    return <p key={link}><a href={link} target="_new">Get this book!</a></p>
+                })}
             </div>
         );
     }
