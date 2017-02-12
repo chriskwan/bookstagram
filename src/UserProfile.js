e08223fb604c9ec5075fd4b9ef57a258eeb88783
Chris Kwan <chriswkwan@gmail.com>
2017-02-11 20:22:13 -0500

    Add complex grid layout

--- src/UserProfile.js
+++ src/UserProfile.js
@@ -1,11 +1,62 @@
 import React, { Component } from 'react';
 import Avatar from 'material-ui/Avatar';
+import { GridList, GridTile } from 'material-ui/GridList';
 import RaisedButton from 'material-ui/RaisedButton';
 
 import placeholderImage from './cat_reading.jpg';
 
 class UserProfile extends Component {
     render() {
+        const styles = {
+            root: {
+                display: 'flex',
+                flexWrap: 'wrap',
+                justifyContent: 'space-around'
+            },
+            gridList: {
+                width: 500,
+                height: 450,
+                overflowY: 'auto'
+            }
+        };
+
+        const placeholderImageUrl = './cat_reading.jpg';
+
+        const tilesData = [{
+            img: placeholderImageUrl,
+            title: 'Breakfast',
+            featured: true
+        },
+        {
+            img: placeholderImageUrl,
+            title: 'Tasty burger'
+        },
+        {
+            img: placeholderImageUrl,
+            title: 'Camera'
+        },
+        {
+            img: placeholderImageUrl,
+            title: 'Morning',
+            featured: true
+        },
+        {
+            img: placeholderImageUrl,
+            title: 'Hats'
+        },
+        {
+            img: placeholderImageUrl,
+            title: 'Honey'
+        },
+        {
+            img: placeholderImageUrl,
+            title: 'Vegetables'
+        },
+        {
+            img: placeholderImageUrl,
+            title: 'Water plant'
+        }];
+
         const userData = {
             name: 'Ana',
             numBooks: 422,
@@ -16,6 +67,20 @@ class UserProfile extends Component {
         const style = {
             margin: 12,
         };
+
+        const tiles = tilesData.map((tile) => {
+            return (
+                <GridTile
+                    key={tile.img + tile.title}
+                    title={tile.title}
+                    titlePosition="top"
+                    cols={tile.featured ? 2 : 1}
+                    rows={tile.featured ? 2 : 1}
+                >
+                    <img src={require(tile.img)} alt="book"/>
+                </GridTile>
+            );
+        });
 
         return (
             <div>
@@ -29,6 +94,17 @@ class UserProfile extends Component {
                 {userData.numFollowing} following
 
                 <RaisedButton label="edit profile" style={style}/>
+
+                <div style={styles.root}>
+                    <GridList
+                        cols={2}
+                        cellHeight={200}
+                        padding={1}
+                        style={styles.gridList}
+                    >
+                        {tiles}
+                    </GridList>
+                </div>
             </div>
         );
     }
